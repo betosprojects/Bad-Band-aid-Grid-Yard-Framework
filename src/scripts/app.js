70bc16de7e1d99a5835b4cddf9b7dd821c4a0ab2
betosprojects <firmetechniquez01@gmail.com>
2017-07-29 12:41:56 -0700

    Hamburger Toggle

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,18 @@
+// Nav Menu Toggle:
+// Documen Content Loader (document.ready):
 
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  var menuIcon = document.querySelector('.hamburger-icon');
+  var navSection = document.querySelector('.nav-section');
+  menuIcon.addEventListener('click', function () {
+
+    if (navSection.classList.contains('close')) {
+      navSection.classList.remove('close');
+    } else {
+      navSection.classList.add('close');
+    }
+  }, false);
+
+});
+// END of document.ready
--- src/scripts/ts/app.ts
+++ src/scripts/ts/app.ts
@@ -1,53 +1,39 @@
-// Dear future Mr. Forgetsalot me:) --------------------------
-// Install it as a Gulp dev-dependencie or globally:
-// npm install -g typescript@next or yarn global add typescript@next
+// -=-=-=--=-=-==-=-=-=-=-=-=- jQuery START -=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Nav Menu Toggle:
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=- jQuery Notes =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Source:
-// https://basarat.gitbooks.io/typescript/content/docs/why-typescript.html
-// Declared definition for TypeScript to handle Jquery normally.TypeScript expects
-// you to declare (i.e. use var somewhere) before you use a variable. As a quick fix
-// you can tell TypeScript that there is indeed something called $ :
-//
+// declare var $:any;
 
-declare var $:any;
+// $(document).ready(function(){
 
-// Or this basic example definition to provide more information to help protect you from errors:
+//   $('.hamburger-icon').on('click', function() {
+//   if ($('.nav-section').hasClass('close')) {
+//   $('.nav-section').removeClass('close');
+//   } else {
+//   $('.nav-section').addClass('close');
+//   }
+//   });
 
-// declare var $: {
-//   (selector: string): any;
-// }
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Currently Experimenting =-=-=-=-=-=-=-=-=-=-=-
-// I used jQuery for the "hamburger" icon to toggle. Paste this on the regular app.js file
-// if not using TypScript to compile. Having the framework clean as possible for future use
-// or having to use another way or method, that's why I didn't include libraries (yet)..:
-
-$(document).ready(function(){
-
-// --------------------------- Hamburger-Icon Toggle ---------------------------
-
-  $('.hamburger-icon').on('click', function() {
-  if ($('.nav-section').hasClass('open')) {
-  $('.nav-section').removeClass('open');
-  } else {
-  $('.nav-section').addClass('open');
-  }
-  });
-
-
-
-});
+// });
 //END of document.ready
 
 
-
-
 // -=-=-=-=-=-=-=-=-=-=-=-=- Regular JavaScript =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Nav Menu Toggle:
 
-document.addEventListener('DOMContentLoaded', function(event) {
+  // Documen Content Loader (document.ready):
 
-// JS goes here
+  document.addEventListener("DOMContentLoaded", function (event) {
 
-});
+    var menuIcon = document.querySelector('.hamburger-icon');
+    var navSection = document.querySelector('.nav-section');
+    menuIcon.addEventListener('click', function () {
+
+      if (navSection.classList.contains('close')) {
+        navSection.classList.remove('close');
+      } else {
+        navSection.classList.add('close');
+      }
+    }, false);
+
+  });
 // END of document.ready
