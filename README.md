62ac22c8325d1f32de9c364c5c1dc6cda0b710b4
betosprojects <firmetechniquez75@gmail.com>
2017-12-29 13:07:34 -0800

    JS footer year

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-# Bad Band-aid CSS Grid Framework
+# Bad Band-aid CSS Grid Template Areas Framework
 
-### This is an ongoing, responsive CSS Grid Layout framework project.
+**This is an ==ongoing==, responsive CSS Grid named template areas project.**
 ***
-My attempt on creating CSS Grid Layout framework.  [CSS Grid Layout](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Basic_Concepts_of_Grid_Layout) introduces a two-dimensional grid system to CSS. In addition, I gave it a bit of Flexbox to a full screen page and the nav links section as a reference of it as well.
+ [CSS Grid Layout](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Basic_Concepts_of_Grid_Layout) introduces a two-dimensional grid system to CSS. In addition, I gave it a bit of Flexbox to a full screen page and the nav links section as a reference of it as well.
 ***
 ![Framework Snapshot](preview.jpg  "Thee Grid Yard Framework")
 Indexed and structured with SCSS ([Sass](http://sass-lang.com/)) files and directories.
 
-**TODO**:
-- Create sample content -
-- Organize/Dry CSS and media queires as usual -
-- Documentation maybe? -
 
 ***
 No attribution required but you can buy me some coffee if you like. :)
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,37 @@
-// Nav Menu Toggle:
-
 // DOM Content Loader:
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
-    var eventClick = document.querySelector('.hamburger-icon');
-    var targetSelector = document.querySelector('.nav-section');
-    eventClick.addEventListener('click', function () {
-        if (targetSelector.classList.contains('toggle')) {
-            targetSelector.classList.remove('toggle');
-        }
-        else {
-            targetSelector.classList.add('toggle');
-        }
-    }, false);
+  // ------------------Nav Menu Toggle:
+
+  var eventClick = document.querySelector('.hamburger-icon');
+  var targetSelector = document.querySelector('.nav-section');
+  eventClick.addEventListener('click', function() {
+    if (targetSelector.classList.contains('toggle')) {
+      targetSelector.classList.remove('toggle');
+    } else {
+      targetSelector.classList.add('toggle');
+    }
+  }, false);
+  // ------------------ END Nav Menu Toggle:
+
+
+
+
+
+  // ---------------------------- Footer Year:
+
+  var thisYear = new Date();
+  document.querySelector("#footYear").innerHTML = thisYear.getFullYear();
+
+  // ------------------------- END Footer Year
+
+
+
+
+
+
+
+
 
 });
 // END of DOM Content Loader
